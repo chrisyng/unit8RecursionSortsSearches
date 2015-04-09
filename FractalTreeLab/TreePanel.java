a6cf2be1d40339677a62cbdc2810f2ebcd325ec5
chrisyng <cyng@NN123-HA-15.HS.sd203.org>
2015-04-09 12:22:49 -0500

    asdf

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -12,11 +12,12 @@ public class TreePanel extends JPanel
     private final int PANEL_WIDTH = 400;
     private final int PANEL_HEIGHT = 400;   
 
-    private final int TOPX = 200, TOPY = 360;
+    private final int TOPX = 200, TOPY = 300;
     private final int BOTTOMX = 200, BOTTOMY = 380;
-    private final int theta = 15;
-
+    private final int theta = 10;
+    private final double treeRatio = 0.8;
     private int current; //current order
+    private final int initLength = BOTTOMY-TOPY;
 
     //-----------------------------------------------------------------
     //  Sets the initial fractal order to the value specified.
@@ -34,22 +35,22 @@ public class TreePanel extends JPanel
     //  intermediate points are computed, and each line segment is
     //  drawn as a fractal.
     //-----------------------------------------------------------------
-    public void drawFractal (int order, int x1, int y1, int x2, int y2, Graphics page)
+    public void drawFractal (int order, int x1, int y1, int x2, int y2, int angle, Graphics page)
     {
-        int deltaX, deltaY, x3, y3, x4, y4;
-
+        int x3, y3, x4, y4;         
+        page.drawLine (x1, y1, x2, y2);
+        int branchLength = (int) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
         if (order == 1){
             return;
         }
         else
-        {
-            page.drawLine (x1, y1, x2, y2);
-            x3 = x2 + (int) Math.sin(Math.toRadians(theta*order));
-            y3 = y2 + (int) Math.cos(Math.toRadians(theta*order));
-            x4 = x2 + (int) Math.sin(Math.toRadians(theta*order));
-            y4 = y2 + (int) Math.cos(Math.toRadians(theta*order));
-            drawFractal(order-1, x2, y2, x3, y3, page);
-            drawFractal(order-1, x2, y2, x4, y4, page);
+        {            
+            x3 = x2 + (int) (branchLength*Math.sin(Math.toRadians(angle)));
+            y3 = y2 - (int) (branchLength*Math.cos(Math.toRadians(angle)));
+            x4 = x2 - (int) (branchLength*Math.sin(Math.toRadians(angle)));
+            y4 = y2 - (int) (branchLength*Math.cos(Math.toRadians(angle)));
+            drawFractal(order-1, x2, y2, x3, y3, angle+theta, page);
+            drawFractal(order-1, x2, y2, x4, y4,angle+theta, page);
         }
     }
 
@@ -59,10 +60,8 @@ public class TreePanel extends JPanel
     public void paintComponent (Graphics page)
     {
         super.paintComponent (page);
-
         page.setColor (Color.green);
-
-        drawFractal (current, BOTTOMX, BOTTOMY, TOPX, TOPY, page);        
+        drawFractal (current, BOTTOMX, BOTTOMY, TOPX, TOPY, 20, page);        
     }
 
     //-----------------------------------------------------------------
